c69b7e6ac12acedc1e17b7939616565004b1ecbc
Rohan Burad <rohanburadtech@gmail.com>
2026-03-29 19:53:28 +0530

    Create 01_Hello_World.py

---
+++ 01_Logic_Challenge/01_Hello_World.py
@@ -0,0 +1,6 @@
+# Task 1: Hello World
+print("Hello, Python Logic Lab!")
+print("My Name is Rohan Burad")
+print("Student of Silver Oak University and I am here to Continue my Passion with Computers and Tech")
+print("I am a Student of Diploma CSE-Artificial-Intelligence and Machine Learning")
+print(f"It's our 3rd Sem , we are introduced to our core Language Python")
